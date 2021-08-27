71884528e78c5d368b775ec259475562f89d4a61
allyviomahardhika <allyviomahardhika@peopleshift.id>
2021-08-28 00:20:38 +0700

    materi ke 5

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Text, View, Button } from 'react-native'
 import FlexBox from './pages/FlexBox'
 import PropsDinamis from './pages/PropsDinamis'
 import State from './pages/State'
+import DetailsScreen from './pages/DetailsScreen'
+import CallApiVanilla from './pages/CallApiVanilla'
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -11,12 +13,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 const App = () => {
-  return <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Details" component={DetailsScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
+  return (
+    < CallApiVanilla />
+  )
+  //   return <NavigationContainer>
+  //     <Stack.Navigator>
+  //       <Stack.Screen name="Home" component={HomeScreen} />
+  //       <Stack.Screen name="Details" component={DetailsScreen} />
+  //     </Stack.Navigator>
+  //   </NavigationContainer>
 }
 
 const HomeScreen = ({ navigation }) => {
@@ -26,23 +31,6 @@ const HomeScreen = ({ navigation }) => {
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-const DetailsScreen = ({ navigation }) => {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.push('Details')}
-      />
-      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-      <Button
-        title="Go back to first screen in stack"
-        onPress={() => navigation.popToTop()}
       />
     </View>
   );
